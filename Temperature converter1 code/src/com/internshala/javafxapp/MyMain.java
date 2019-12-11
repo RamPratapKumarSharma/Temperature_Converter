fb2cef3794fd77460eace8b673707645612a53e0
RamPratapKumarSharma <rampratap.rs61@gmail.com>
2019-12-11 16:50:01 +0530

    Temperature_Converter

---
+++ Temperature converter1 code/src/com/internshala/javafxapp/MyMain.java
@@ -0,0 +1,102 @@
+package com.internshala.javafxapp;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class MyMain extends Application {
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		launch(args);
+	}
+
+	@Override
+	public void init() throws Exception {
+		System.out.println("init"); // Initialize your application
+		super.init();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		System.out.println("start");    // Starts an application
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("app_layout.fxml"));
+		VBox rootNode = loader.load();
+
+		MenuBar menuBar = createMenu();
+		rootNode.getChildren().add(0, menuBar);
+		primaryStage.setResizable(false);
+
+		Scene scene = new Scene(rootNode);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Temperature Converter");
+		primaryStage.show();
+	}
+
+	private MenuBar createMenu() {
+
+		// File Menu
+		Menu fileMenu = new Menu("File");
+
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+		MenuItem quitMenuItem = new MenuItem("Quit");
+
+		quitMenuItem.setOnAction(event -> {
+			Platform.exit();
+			System.exit(0);
+		});
+
+		fileMenu.getItems().addAll(separatorMenuItem, quitMenuItem);
+
+		// Help Menu
+		Menu helpMenu = new Menu("Help");
+		MenuItem aboutApp = new MenuItem("About");
+
+		aboutApp.setOnAction(event -> aboutApp());
+
+		helpMenu.getItems().addAll(aboutApp);
+
+		// Menu Bar
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+		return menuBar;
+	}
+
+	public static void aboutApp() {
+
+		Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+		alertDialog.setTitle("About Temperature Converter");
+		alertDialog.setHeaderText("Developed and Designed By : \n Ram Pratap Sharma");
+		alertDialog.setContentText("To use the temperature converter, select the temperature converter from the menu and enter the number you want to convert to other units of temperature like Celsius to Fahrenheit and Fahrenheit to Celsius.The results will be shown instantly.");
+
+		ButtonType yesBtn = new ButtonType("Yes");
+		//ButtonType noBtn = new ButtonType("No");
+
+		alertDialog.getButtonTypes().setAll(yesBtn);
+
+		Optional<ButtonType> clickedBtn = alertDialog.showAndWait();
+
+		if (clickedBtn.isPresent() && clickedBtn.get() == yesBtn) {
+			System.out.println("Yes Button Clicked !");
+		} else {
+			System.out.println("No Button Clicked !");
+		}
+	}
+
+	@Override
+	public void stop() throws Exception {
+
+		System.out.println("stop"); // Called when application is stopped and is about to shut down
+		super.stop();
+	}
+}
